4a8e41a166c7113dd0ae09f5c80ae1bf394dd9fb
Tony Lei <tony.lei003@gmail.com>
2024-04-17 14:51:42 -0700

    parse return statements

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-	let x 5;
-	let = 10;
-	let 838383;`
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;`
 
 	l := lexer.New(input)
 	p := New(l)
@@ -66,6 +66,42 @@ func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got %d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement, got=%T instead", stmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+				returnStmt.TokenLiteral(),
+			)
+		}
+	}
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
